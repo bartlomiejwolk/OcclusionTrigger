dee0ff2a4ea72226bd8de07c932c3d10332a2d39
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-08 11:30:00 +0200

    Draw `Target` field

--- Editor/TriggerEditor.cs
+++ Editor/TriggerEditor.cs
@@ -1,9 +1,32 @@
 ﻿using UnityEditor;
+using UnityEngine;
 
 namespace OcclusionTrigger {
 
     [CustomEditor(typeof (Trigger))]
     public sealed class TriggerEditor : Editor {
+
+        private Trigger Script { get; set; }
+
+        private SerializedProperty targetTransform;
+
+        private void OnEnable() {
+            Script = (Trigger) target;
+
+            targetTransform = serializedObject.FindProperty("targetTransform");
+        }
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(
+                targetTransform,
+                new GUIContent(
+                    "Target",
+                    "Target transform to check for occlusion."));
+
+            serializedObject.ApplyModifiedProperties();
+        }
 
     }
 
--- Trigger.cs
+++ Trigger.cs
@@ -3,7 +3,28 @@ using System.Collections;
 
 namespace OcclusionTrigger {
 
+    /// <summary>
+    /// Performs an action when specified transform is occluded.
+    /// </summary>
+    /// <remarks>
+    /// Put it on the camera component.
+    /// </remarks>
     public sealed class Trigger : MonoBehaviour {
+
+        #region FIELDS
+
+        [SerializeField]
+        private Transform targetTransform;
+        #endregion
+
+        #region PROPERTIES
+
+        public Transform TargetTransform {
+            get { return targetTransform; }
+            set { targetTransform = value; }
+        }
+
+        #endregion
 
     }
 }
